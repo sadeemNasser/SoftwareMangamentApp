f8b4d0ccf1889fc67783c44c9c2df06188da0c6b
sadeemNasser <sadoomnasser@gmail.com>
2020-04-23 19:27:49 +0300

    -

--- app/src/main/java/com/example/softwaremangamentapp/SignupActivity.java
+++ app/src/main/java/com/example/softwaremangamentapp/SignupActivity.java
@@ -128,8 +128,8 @@ public class SignupActivity extends AppCompatActivity {
             _emailText.setError(null);
         }
 
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            _passwordText.setError("between 4 and 10 alphanumeric characters");
+        if (password.isEmpty() || password.length() < 6 ) {
+            _passwordText.setError("password should be 6 or more ");
             valid = false;
         } else {
             _passwordText.setError(null);
